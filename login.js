a1c3a2d5e880600fa65ce7dee580e2004cc16bfb
Viicked <bartekrzadzi1@wp.pl>
2025-05-12 19:51:21 +0200

    Update login.js

--- login.js
+++ login.js
@@ -42,11 +42,11 @@ async function Login() {
         const responseData = await response.json();
 
         if (!responseData.IsUser) {
-            errorElement.innerText = "nieznana nazwa użytkownika";
+            errorElement.innerText = "Nieznana nazwa użytkownika";
             return;
         }
         if (!responseData.IsValid) {
-            errorElement.innerText = "błędne hasło";
+            errorElement.innerText = "Błędne hasło";
             return;
         }
     } catch (error) {
@@ -62,61 +62,5 @@ async function encryptPassword(password) {
 
 function FlashRed(obj) {
     obj.style.backgroundColor = "rgb(192, 30, 30)";
-    setTimeout(() => obj.style.backgroundColor = "rgb(216, 216, 216)", 300);
-}async function Login() {
-    let username = document.getElementById("username");
-    let password = document.getElementById("password");
-    let error = document.getElementById("error-space");
-
-    if(username.value == "") {
-        FlashRed(username);
-        error.innerText = "Puste pole nazwa użytkownika";
-        return;
-    }
-    if(password.value == "") {
-        FlashRed(password);
-        error.innerText = "Puste pole hasło";
-        return;
-    }
-    if(username.value.length < 5) {
-        FlashRed(username)
-        error.innerText = "Nazwa użytkownika nie może być krótsza niż 5 znaków";
-        return;
-    }
-    if(password.value.length < 5) {
-        FlashRed(password)
-        error.innerText = "Hasło nie może być krótsze niż 5 znaków";
-        return;
-    }
-    error.innerText = "";
-
-    let encrypted_password_obj = new jsSHA("SHA-512", "TEXT", {numRounds: 1});
-    encrypted_password_obj.update(password.value);
-    let encrypted_password = encrypted_password_obj.getHash("HEX");
-
-    const response_text = await fetch("https://127.0.0.1/post", {
-        method: "POST",
-        body: JSON.stringify({
-            action: "login",
-            username: username.value,
-            password: encrypted_password
-        })
-    });
-
-    let response = JSON.parse(response_text);
-    if(!response.IsUser) {
-        error.innerText = "nieznana nazwa użytkownika";
-        return;
-    } 
-    if(!response.IsValid) {
-        error.innerText = "błędne hasło";
-        return;
-    }
-    
-    // coś dalej
-}
-
-function FlashRed(obj) {
-    obj.style.backgroundColor = "rgb(192, 30, 30)";
-    setTimeout(() => obj.style.backgroundColor = "rgb(216, 216, 216)", 300);
+    setTimeout(() => obj.style.backgroundColor = "", 300);
 }
